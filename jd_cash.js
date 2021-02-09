82c3a15f2350655bad5b43ccf3e2f4824b0eddc8
shuye73 <78575544+shuye73@users.noreply.github.com>
2021-02-09 22:22:03 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -266,7 +266,7 @@ function readShareCode() {
           console.log(`${$.name} API请求失败，将切换为备用API`)
           $.get({url: `https://raw.githubusercontent.com/shuye73/RandomShareCode/master/JD_Cash.json`, 'timeout': 10000},(err, resp, data)=>{
           data = JSON.parse(data);
-          console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)}
+          console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)})
         } else {
           if (data) {
             console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
