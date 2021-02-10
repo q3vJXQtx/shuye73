53269390e14304c6ddd2fb92b8a4431c779f3cfa
shuye73 <78575544+shuye73@users.noreply.github.com>
2021-02-10 14:29:31 +0800

    移除过期活动，修改东东超市运行频率

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -41,7 +41,7 @@
 # 京东天天加速
 8 */3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 # 东东超市
-11 1-23/5 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
+11 */6 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
 # 取关京东店铺商品
 55 23 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1
 # 京豆变动通知
