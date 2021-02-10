c75814b3bab02dc745355c81bbc5122a4aaf8f02
shuye73 <78575544+shuye73@users.noreply.github.com>
2021-02-10 13:11:50 +0800

    新增京东环球挑战赛
    新增京东环球挑战赛

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,7 +10,8 @@
 0 0,10,15 * * * node /scripts/jd_newYearMoney.js >> /scripts/logs/jd_newYearMoney.log 2>&1
 # 京东压岁钱抢百元卡(活动时间：2021-2-1至2021-2-11)
 0 9,12,16,20 * * * node /scripts/jd_newYearMoney_lottery.js >> /scripts/logs/jd_newYearMoney_lottery.log 2>&1
-
+#环球挑战赛 活动时间：2021-02-02 至 2021-02-22
+0 9,12,20,21 * * * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 
 ##############长期活动##############
 # 签到
