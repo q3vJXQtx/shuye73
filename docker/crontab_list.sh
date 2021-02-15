4fa424ab56db3655a7ab9ddaeb6ddc7de6e2733c
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-15 21:07:01 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,7 +7,7 @@
 # 小鸽有礼2(活动时间：2021年1月28日～2021年2月28日)
 34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
 #环球挑战赛 活动时间：2021-02-02 至 2021-02-22
-5 9,12,20,21 2-22 2 * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
+10 9,12,20,21 2-22 2 * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 
 ##############长期活动##############
 # 京东小魔方(活动时间：2021年1月28日～2021年2月28日)
