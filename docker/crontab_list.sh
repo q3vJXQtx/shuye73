608d0f1dc5c4fc0011521f647331bba407f36913
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-20 23:19:03 +0800

    新增京东价格保护，京东极速版红包，京东极速版
    新增京东价格保护，京东极速版红包，京东极速版

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -103,4 +103,9 @@
 10 * * * *  node /scripts/jx_cfd.js >> /scripts/logs/jx_cfd.log 2>&1
 # 京东试用（默认注释，请配合取关脚本使用）
 #10 0 * * *  node /scripts/jd_try.js >> /scripts/logs/jd_try.log 2>&1
-
+# 京东价格保护
+10 0 */3 * *  node /scripts/jd_priceProtect.js >> /scripts/logs/jd_priceProtect.log 2>&1
+# 京东极速版红包
+40 0,12,18 * * *  node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
+# 京东极速版
+48 0,12,18 * * *  node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
